6a6969323ce6d817c7550a06300caead4daf832f
Roque1211 <roque1211@gmail.com>
2021-05-16 11:45:15 +0200

    Con login operativo, next: intercambio de token y menu inicial

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,16 +1,37 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs";
-import { environment } from "login/src/environments/environment";
+import { CookieService } from "ngx-cookie-service";
 
 @Injectable({
   providedIn: "root"
 })
 export class UsersService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private cookies: CookieService) {}
 
+  // llamada a la API
   login(user: any): Observable<any> {
-
-    return this.http.post("https://localhost:44300/api/Login", user);
+    return this.http.post("https://localhost:44300/api/Login",user);
   }
+
+  // guarda token en una cookie
+  setToken(token: string) {
+    this.cookies.set("token", token);
+  }
+
+  // devuelve token desde una cookie
+  getToken() {
+    return this.cookies.get("token");
+  }
+
+  // recupera user con un id determinado
+  getUser() {
+  //  return this.http.post("https://localhost:44300/api/User",userId);
+  }
+
+  // recupera user actual
+  getUserLogged() {
+    const token = this.getToken();
+    // hay que sacar el id del usuario actual
+    return this.http.get("https://localhost:44300/api/User");  }
 }
